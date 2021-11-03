43cdd7d065821dee403f47731928bab23feb00c1
batlicka <vojta@vagunda.eu>
2021-11-03 16:24:29 +0100

    change in java file

--- main.java
+++ main.java
@@ -1,12 +1,13 @@
 public class starter{
     public Double obvod(double a, double b){
-        return 2*a+2*b;
+        Double var = 2*a+2*b
+        return var;
     }
     public Double obsah(double a, double b){
         return a*b;
     }
 
-    public void Main(){
-
+    public static void main(String args[]){
+        System.out.println("Hello World");
     }
 }
