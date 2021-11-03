eeb3b0fc5ab131e591867e857059a096b131fcb3
batlicka <vojta@vagunda.eu>
2021-11-03 16:22:22 +0100

    basic java commit

---
+++ main.java
@@ -0,0 +1,12 @@
+public class starter{
+    public Double obvod(double a, double b){
+        return 2*a+2*b;
+    }
+    public Double obsah(double a, double b){
+        return a*b;
+    }
+
+    public void Main(){
+
+    }
+}
